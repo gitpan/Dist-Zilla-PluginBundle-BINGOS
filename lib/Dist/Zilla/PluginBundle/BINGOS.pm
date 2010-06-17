31a91310e50122d761e5cb32cce90fa9aad95534
Chris Williams <chris@bingosnet.co.uk>
2010-06-17 21:30:50 +0000

    Import of BINGOS/Dist-Zilla-PluginBundle-BINGOS-0.02 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-PluginBundle-BINGOS
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         BINGOS/Dist-Zilla-PluginBundle-BINGOS-0.02.tar.gz
    gitpan-cpan-author:       BINGOS
    gitpan-cpan-maturity:     released

---
+++ lib/Dist/Zilla/PluginBundle/BINGOS.pm
@@ -0,0 +1,96 @@
+package Dist::Zilla::PluginBundle::BINGOS;
+BEGIN {
+  $Dist::Zilla::PluginBundle::BINGOS::VERSION = '0.02';
+}
+
+# ABSTRACT: BeLike::BINGOS when you build your dists
+
+use Moose;
+with 'Dist::Zilla::Role::PluginBundle::Easy';
+
+sub configure {
+  my $self = shift;
+  $self->add_bundle('Basic');
+  $self->add_plugins(
+    'MetaJSON',
+    'PodSyntaxTests',
+    'PodCoverageTests',
+    'PkgVersion',
+    'GithubMeta',
+    'ReadmeFromPod',
+    'PodWeaver',
+    'CompileTests',
+    [ 'ChangelogFromGit' => 
+        { file_name => 'Changes', tag_regexp => '^\d' } 
+    ],
+  );
+
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+qq[BELIKE::BINGOS];
+
+
+__END__
+=pod
+
+=head1 NAME
+
+Dist::Zilla::PluginBundle::BINGOS - BeLike::BINGOS when you build your dists
+
+=head1 VERSION
+
+version 0.02
+
+=head1 SYNOPSIS
+
+   # in dist.ini
+   [@BINGOS]
+
+=head1 DESCRIPTION
+
+This is a L<Dist::Zilla> PluginBundle.  It is roughly equivalent to the
+following dist.ini:
+
+  [@Basic]
+
+  [MetaJSON]
+  [PodSyntaxTests]
+  [PodCoverageTests]
+
+  [PodWeaver]
+  [PkgVersion]
+  [GithubMeta]
+
+  [ChangelogFromGit]
+  file_name = Changes
+  tag_regexp = ^\d
+
+  [ReadmeFromPod]
+  [CompileTests]
+
+=head2 METHODS
+
+=over
+
+=item C<configure>
+
+See L<Dist::Zilla::PluginBundle::Easy>.
+
+=back
+
+=head1 AUTHOR
+
+Chris Williams <chris@bingosnet.co.uk>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2010 by Chris Williams.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
